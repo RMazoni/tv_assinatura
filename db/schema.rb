c00816799cd269a370445a1c96bd3aec397141d4
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-07-20 11:19:48 -0300

    Complete assiciations between initial models
    Added the associations inside the models, using
    belongs_to, has_many and has_and_belongs_to_many.

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_07_20_140954) do
+  create_table "additionals", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.date "birth_date"
+    t.integer "plan_id", null: false
+    t.integer "package_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["package_id"], name: "index_clients_on_package_id"
+    t.index ["plan_id"], name: "index_clients_on_plan_id"
+  end
+
+  create_table "packages", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.integer "plan_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plan_id"], name: "index_packages_on_plan_id"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "clients", "packages"
+  add_foreign_key "clients", "plans"
+  add_foreign_key "packages", "plans"
+end

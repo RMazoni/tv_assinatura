501cf2e275469f739c53033810de99c002e37563
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-07-21 16:48:44 -0300

    Ad birth_date validation
    Client must be at least 18 years old.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,21 @@
 class Client < ApplicationRecord
   validates :name, presence: true
   validates :birth_date, presence: true
+  validate :must_be_at_least_18_years_old
+
+
   belongs_to :plan, optional: true
   belongs_to :package, optional: true
+
+
+
+  private
+
+  def must_be_at_least_18_years_old
+    return unless birth_date.present?
+
+    if birth_date > 18.years.ago.to_date
+      errors.add(:birth_date, "Client must be at least 18 years old.")
+    end
+  end
 end

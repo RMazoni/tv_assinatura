06ce9e0a9207e50eed1d14c002d249ebf4617e94
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-07-21 17:25:37 -0300

    Add validation to package. At least one additional

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,6 +1,8 @@
 class Package < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
+  validates :additionals, length: { minimum: 1, message: "Must have at least one additional" }
+
   belongs_to :plan
   has_and_belongs_to_many :additionals
 end

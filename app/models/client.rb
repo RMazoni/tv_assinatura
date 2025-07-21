5bd1ef7bd8fce559d740fd6e806a728889593563
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-07-21 18:02:33 -0300

    Add validation for plan or package
    Still need to update the views allow subscription.

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,11 +2,11 @@ class Client < ApplicationRecord
   validates :name, presence: true
   validates :birth_date, presence: true
   validate :must_be_at_least_18_years_old
+  validate :can_have_plan_or_package_not_both
 
 
   belongs_to :plan, optional: true
   belongs_to :package, optional: true
-
 
 
   private
@@ -16,6 +16,12 @@ class Client < ApplicationRecord
 
     if birth_date > 18.years.ago.to_date
       errors.add(:birth_date, "Client must be at least 18 years old.")
+    end
+  end
+
+  def can_have_plan_or_package_not_both
+    if plan.present? && package.present?
+      errors.add(:base, "Only plan or package allowed.")
     end
   end
 end

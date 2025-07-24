4a4833385edd24f2d70be2d9578b0b105acad6df
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-07-24 18:21:03 -0300

    Add Package auto calculation when price is blank
    Used after_save callback and update_column, because
    of the HABTM association. To fetch the ID at the
    additionals_packages table, the package ID is needed.
    So the app needs to create the package with blank price
    and update the column with the method using the after_save.

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,8 +1,19 @@
 class Package < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
+  validates :plan, presence: true
   validates :additionals, length: { minimum: 1, message: "Must have at least one additional" }
 
   belongs_to :plan
   has_and_belongs_to_many :additionals
+
+  after_save :package_price_calculator # needs to be after_save bacause of HABTM association
+
+  private
+
+  def package_price_calculator
+    if price.blank?
+      calculated_price = plan.price + additionals.sum(:price)
+      update_column(:price, calculated_price)
+    end
+  end
 end

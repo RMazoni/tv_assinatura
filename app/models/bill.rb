2e6e0190bdddf62c9857d5947b7caeda2e023deb
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-08-03 15:10:52 -0300

    Add validations to Bill model

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,10 @@
 class Bill < ApplicationRecord
   belongs_to :subscription
   belongs_to :additional, optional: true
+
+  validates :bill_type, presence: true, inclusion: { in: %w[plan package additional] }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :due_date, presence: true
+  validates :additional, presence: true, if: -> { bill_type == "additional" }
+  validates :additional, absence: true, unless: -> { bill_type == "aditional" }
 end

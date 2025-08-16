af669cebe6be5876f8199f3e77961fcdb918bddc
Rodrigo Mazoni <mazoni.rodrigo94@gmail.com>
2025-08-16 19:38:48 -0300

    Fix subscription. Plan and package were allowed
    Subscription can have either plan or package.

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,6 @@ class Client < ApplicationRecord
   validates :name, presence: true
   validates :birth_date, presence: true
   validate :must_be_at_least_18_years_old
-  validate :can_have_plan_or_package_not_both
 
   has_one :subscription, dependent: :destroy
 
@@ -17,9 +16,4 @@ class Client < ApplicationRecord
     end
   end
 
-  def can_have_plan_or_package_not_both
-    if plan.present? && package.present?
-      errors.add(:base, "Only plan or package allowed.")
-    end
-  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,10 +1,20 @@
 class Subscription < ApplicationRecord
   validates :package_id, presence: false
   validates :plan_id, presence: false
+  validate :can_have_plan_or_package_not_both
 
   belongs_to :client
   belongs_to :package
   belongs_to :plan
   has_and_belongs_to_many :additionals
   has_many :bills, dependent: :destroy
+
+  private
+
+  def can_have_plan_or_package_not_both
+    if plan.present? && package.present?
+      errors.add(:base, "Only plan or package allowed.")
+    end
+  end
+
 end
